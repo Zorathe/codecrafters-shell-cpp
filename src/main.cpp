b57978e9f3bc9204d5108c3a5c91c969a4e93f64
Zorathe <dathomaschen@gmail.com>
2026-05-26 01:04:30 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,14 @@ int main() {
     }
     if(input.substr(0,input.find(" ")) == "echo"){
       std::cout << input.substr(5) << std::endl;
+    }else if(input.substr(0,input.find(" ") == "type")){
+      if(input.substr(input.find(" "),4 == "echo")){
+        std::cout << "echo is a shell builtin" << std::endl;
+      }else if(input.substr(input.find(" "),4 == "exit")){
+        std::cout << "exit is a shell builtin" << std::endl;
+      }else{
+        std::cout << input.substr(input.find(" ")) << ": not found" << std::endl;
+      }
     }else{
       std::cout << input << ": command not found" << std::endl;
     }
