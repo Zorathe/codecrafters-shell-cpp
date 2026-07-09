5a6afc9dca6ab00b409a8c72ecff26dfcd32c399
Zorathe <dathomaschen@gmail.com>
2026-07-08 23:01:48 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -267,7 +267,7 @@ void reap_jobs(bool explicitly_called){
     for(int i = 0; i < jobs.size(); i++){
       int temp = 0;
       if(jobs[i].running){
-        if(temp > 4)
+        if(temp > 2)
         std::cout << "was start runnn" << std::endl;
         temp++;
         int status;
